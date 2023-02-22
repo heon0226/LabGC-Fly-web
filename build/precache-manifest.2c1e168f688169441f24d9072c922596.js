cffecb2d57f2a66002301dd8b982c514f8e6ffec
heon <zsewa0@hallym.ac.kr>
2023-02-22 17:21:06 +0900

    append build directory

---
+++ build/precache-manifest.2c1e168f688169441f24d9072c922596.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d46736bb8c61f6e32a8ec91b1c88e200",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49feff8596502efcf4d9",
+    "url": "/static/css/2.4173a4ba.chunk.css"
+  },
+  {
+    "revision": "49feff8596502efcf4d9",
+    "url": "/static/js/2.5a2bf3eb.chunk.js"
+  },
+  {
+    "revision": "a64e05d89146aa4583c536fb462eb1eb",
+    "url": "/static/js/2.5a2bf3eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6763e92e2eee7c7a5b1",
+    "url": "/static/js/main.dbdb2126.chunk.js"
+  },
+  {
+    "revision": "ea7e859ac2f54974f198",
+    "url": "/static/js/runtime-main.926d109b.js"
+  }
+]);
